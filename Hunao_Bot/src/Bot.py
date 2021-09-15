44ffa9d400e12fb06eaf0f6d022e72efbede9585
Muntasir Chowdhury Mridul <darkblade6952@gmail.com>
2021-09-15 22:36:53 +0600

    Join and Leave added

--- Hunao_Bot/src/Bot.py
+++ Hunao_Bot/src/Bot.py
@@ -1,21 +1,39 @@
 import discord
-import nest_asyncio
+#import requests
+#import json
+from discord.ext import commands
 
+import nest_asyncio
 nest_asyncio.apply()
-client = discord.Client()
+
+#intents = discord.Intents.default()
+#intents.members = True
+
+client = commands.Bot(command_prefix='-')
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
     
 
-@client.event
-async def on_message(message):
-     if message.author == client.user:
-         return
-     
-     if message.content.startswith('Kire vatija?'):
-         await message.channel.send('Ho kaka asi!')
-         
+@client.command(pass_context = True)
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    
+    else:
+        await ctx.send("Vatija tumi voice channel e nai,\n You must be in a voice channel to run this command")
+    
+@client.command(pass_context = True)
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Ami jaiga. Leaving forever :)")
+    
+    else:
+        await ctx.send("Ami ghumaitasi vatija")
+       
+          
 
 client.run('ODg3NzA3ODU1MDU3MjA3MzU3.YUIEXQ.DMtNn64HImdC4Ei47lMdnH7-l7U')
